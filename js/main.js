db971cb9aa9786a151ff3ba60b7d0ae92e755166
Nikolay <n_vinok98@mail.ru>
2021-03-09 20:07:02 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
+$(function () {
+    $('.banner-section__slider').slick({
+        dots: true,
+        autoplay: true,
+        prevArrow: ' <button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow_left.svg" alt=""></button>',
+        nextArrow: ' <button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow_right.svg" alt=""></button>'
 
+    });
 });
